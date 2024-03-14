d1b254e5276786c531f0a8e1df045b34e5d847d9
Kumar Shivesh <116376181+kumarshivesh@users.noreply.github.com>
2024-03-14 23:42:58 +0530

    Added bg-changer folder

---
+++ bg-changer/src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+  return (
+    <div className='background'
+    style={{backgroundColor: color}}
+    >
+      <div className='bar-position'>
+        <div className='bar-content'>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'red'}}
+          onClick={() => setColor('red')}
+          >Red</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'green'}}
+          onClick={() => setColor('green')}
+          >Green</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'blue'}}
+          onClick={() => setColor('blue')}
+          >Blue</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'olive'}}
+          onClick={() => setColor('olive')}
+          >Olive</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'gray'}}
+          onClick={() => setColor('gray')}
+          >Gray</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'yellow'}}
+          onClick={() => setColor('yellow')}
+          >Yellow</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'pink'}}
+          onClick={() => setColor('pink')}
+          >Pink</button>
+          <button className='buttons'
+          style={{backgroundColor: 'purple'}}
+          onClick={() => setColor('purple')}
+          >Purple</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'lavender'}}
+          onClick={() => setColor('lavender')}
+          >Lavender</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'white'}}
+          onClick={() => setColor('white')}
+          >White</button>
+          <button 
+          className='buttons'
+          style={{backgroundColor: 'black'}}
+          onClick={() => setColor('black')}
+          >Black</button>
+        </div>
+      </div>  
+    </div>
+  )
+}
+
+export default App
